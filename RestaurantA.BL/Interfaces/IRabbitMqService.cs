3bd1a37ce101695aadd278f5ab8243945aeae359
StanislavaBandeva <93996233+StanislavaBandeva@users.noreply.github.com>
2022-06-20 18:13:22 +0300

    Add files via upload

---
+++ RestaurantA.BL/Interfaces/IRabbitMqService.cs
@@ -0,0 +1,10 @@
+﻿using RestaurantA.Models;
+using System.Threading.Tasks;
+
+namespace RestaurantA.BL.Interfaces
+{
+    public interface IOrderMqService
+    {
+        Task PublishOrderAsync(Order order);
+    }
+}
---
+++ RestaurantA.DL/IOrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using RestaurantA.Models;
+
+namespace RestaurantA.DL
+{
+    public interface IOrderRepository
+    {
+        Task Add(Order order);
+
+        Task<IEnumerable<IOrderRepository>> GetAll();
+
+        Task<IEnumerable<Order>> GetAllByYear(int Year);
+    }
+}
