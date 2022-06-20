aee72aa2f1b3ffc3cc2e8f935d45bf1608c296c6
StanislavaBandeva <93996233+StanislavaBandeva@users.noreply.github.com>
2022-06-20 18:50:56 +0300

    Add files via upload

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,27 @@
+﻿using RestaurantA.BL.Interfaces;
+using RestaurantA.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace RestaurantA.Controllers
+{
+    [ApiController]
+    [Route("controller")]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IRabbitMqService _rabbitMq;
+
+        public OrdersController(IRabbitMqService rabbitMq)
+        {
+            _rabbitMq = rabbitMq;
+        }
+
+        [HttpPost("Publish Order to RabbitMQ")]
+        public async Task<IActionResult> ProduceOrder([FromBody] Order order)
+        {
+            await _rabbitMq.PublishOrderAsync(order);
+
+            return Ok();
+        }
+    }
+}
